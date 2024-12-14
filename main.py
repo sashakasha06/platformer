595a6ec9d8635694caec5ed5f52a5e92b7a7f4f6
Sasha Gusev <sasha.e.gusev3@yandex.ru>
2024-12-14 17:53:33 +0300

    Platformer

--- main.py
+++ main.py
@@ -1,82 +1,55 @@
-import os
-import sys
-import random
 import pygame
+import sys
 
+# Инициализация Pygame
+pygame.init()
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # Если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+# Устанавливаем размеры экрана
+screen_width = 900
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
 
+# Загружаем фоновое изображение
+bg_image = pygame.image.load('data/bg.png')
 
-class BombSprite(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, mouse_pos):
         super().__init__()
-        self.image = load_image("bomb.png")
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.image = pygame.image.load("data/platform.png")
+        self.rect = self.image.get_rect(topleft=mouse_pos)
 
-    def explode(self):
-        self.image = load_image("boom.png")
-        self.rect.x -= 35
-        self.rect.y -= 31
-
-
-class BombGroup(pygame.sprite.Group):
-    def __init__(self, num_bombs):
+class PlatformGroup(pygame.sprite.Group):
+    def __init__(self):
         super().__init__()
-        self.used_positions = []
-        self.create_bombs(num_bombs)
 
-    def create_bombs(self, num_bombs):
-        while num_bombs > 0:
-            x = random.randint(60, 440)
-            y = random.randint(61, 439)
-            if not self.is_position_used(x, y):
-                bomb = BombSprite(x, y)
-                self.add(bomb)
-                self.used_positions.append((x, y))
-                num_bombs -= 1
-
-    def is_position_used(self, x, y):
-        for value in self.used_positions:
-            if abs(x - value[0]) < 65 and abs(y - value[1]) < 65:
-                return True
-        return False
-
-
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 500, 500
-    screen = pygame.display.set_mode(size)
-
-    bomb_group = BombGroup(num_bombs=20)
-
-    running = True
-    while running:
+# Основной игровой цикл
+def main():
+    moon = pygame.sprite.Sprite()
+    moon.image = pygame.image.load('data/moon.png')
+    moon.rect = moon.image.get_rect()
+    moon.rect.x = 0
+    moon.rect.y = 50
+    v = 25
+    clock = pygame.time.Clock()
+    platforms = PlatformGroup()
+    while True:
         for event in pygame.event.get():
-            # При закрытии окна
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                for bomb in bomb_group:
-                    if bomb.rect.collidepoint(mouse_pos):
-                        bomb.explode()
+                new_platform = Platform(mouse_pos)
+                platforms.add(new_platform)  # Добавляем новую платформу в группу
 
-        screen.fill((255, 255, 255))
-        bomb_group.draw(screen)
+        # Отображаем фоновое изображение
+        delta_time = clock.tick(25) / 1000
+        moon.rect.x += v * delta_time
+        screen.blit(bg_image, (0, 0))
+        screen.blit(moon.image, (((moon.rect.x % 950) - 50), moon.rect.y))
+        platforms.draw(screen)  # Рисуем платформы
         pygame.display.flip()
 
-    pygame.quit()
+# Запускаем игру
+if __name__ == "__main__":
+    main()
